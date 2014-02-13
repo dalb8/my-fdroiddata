b032b7d29cf65a7a24c1e496e5b97c202ef9430b
David Black <david8black@gmail.com>
2014-02-13 18:43:16 +0000

    add gradle-init script

---
+++ tools/gradle-init.sh
@@ -0,0 +1,33 @@
+# Warn about some features of a gradle build
+#! /bin/sh
+echo -e "\033[31m   Wrapper:"
+echo -e "\033[0m"
+grep \.zip gradle/wrapper/gradle-wrapper.properties
+
+echo -e "\n\033[31m   Overall:"
+echo -e "\033[0m"
+grep gradle build.gradle
+
+echo -e "\n\033[31m   Library settings:"
+echo -e "\033[0m"
+cat settings.gradle
+
+echo -e "\n\n\033[31m   Repos:"
+echo -e "\033[0m"
+find . -name build.gradle -print |xargs grep -A 2 repositor
+
+echo -e "\n\n\033[31m   Jars:"
+echo -e "\033[0m"
+find . -name \*jar | grep -v 'gradle\|build\|dexed'
+
+echo -e "\n\n\033[31m   Licence:"
+echo -e "\033[0m"
+ls -1 LICENSE* COPYING | xargs head -n 3 
+#grep -r 'Apache\|GNU\|Copyright' *
+
+echo -e "\n\n\033[31m   Fix:"
+echo -e "\033[0m"
+sed -i 's/-all.zip/-bin.zip/g' gradle/wrapper/gradle-wrapper.properties
+diff ~/fdroiddata/misc/gradlew gradlew
+chmod +x gradlew
+echo " "
