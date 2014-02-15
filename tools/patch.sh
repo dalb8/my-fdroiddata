d490383c475839abd893ecfdcf7d745dad7033d1
David Black <david8black@gmail.com>
2014-02-15 22:41:38 +0000

    script to sign apks via fdroid; add to patch script

--- tools/patch.sh
+++ tools/patch.sh
@@ -1,3 +1,4 @@
 sed -i "/tarball.add(build_dir, tarname, exclude=tarexc)/d" fdroidserver/build.py
 sed -i "s/'gradle'\]\, 'clean'/'gradle'\]\, '--help'/g" fdroidserver/build.py
 #sed -i "/'ant'\, 'clean'/d" fdroidserver/build.py
+sed -i 's/"install"/"install", "-r"/g' fdroidserver/install.py
