78ca3b5e3e01adba1634ace668b50954de1f5dde
David Black <david8black@gmail.com>
2014-02-15 17:13:40 +0000

    add script to tweak fdroidserver

---
+++ tools/patch.sh
@@ -0,0 +1,3 @@
+sed -i "/tarball.add(build_dir, tarname, exclude=tarexc)/d" fdroidserver/build.py
+sed -i "s/'gradle'\]\, 'clean'/'gradle'\]\, '--help'/g" fdroidserver/build.py
+#sed -i "/'ant'\, 'clean'/d" fdroidserver/build.py
