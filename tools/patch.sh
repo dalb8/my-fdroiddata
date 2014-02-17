d2339872617016e5c81306a38c86291ad385109f
David Black <david8black@gmail.com>
2014-02-17 14:30:36 +0000

    fix gloomy dungeons; remove ant clean from fdroidserver

--- tools/patch.sh
+++ tools/patch.sh
@@ -1,4 +1,6 @@
 sed -i "/tarball.add(build_dir, tarname, exclude=tarexc)/d" fdroidserver/build.py
+
 sed -i "s/'gradle'\]\, 'clean'/'gradle'\]\, '--help'/g" fdroidserver/build.py
-#sed -i "/'ant'\, 'clean'/d" fdroidserver/build.py
+sed -i "s/'ant', 'clean'/'ant', '-version'/g" fdroidserver/build.py
+
 sed -i 's/"-s", dev, "install"/"install", "-r"/g' fdroidserver/install.py
