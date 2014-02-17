754e1322f20bd8a3047fd0fd746347e2a67d64ce
David Black <david8black@gmail.com>
2014-02-17 15:04:36 +0000

    update gradlew script

--- tools/gradlew-init.sh
+++ tools/gradlew-init.sh
@@ -16,6 +16,8 @@ grep gradle build.gradle
 
 echo -e "\n\033[31m   Build Tools:"
 echo -e "\033[0m"
+echo 'Available:'
+ls $ANDROID_HOME/build-tools
 grep buildTools $(find -L . -name build.gradle)
 
 echo -e "\n\033[31m   Library settings:"
@@ -26,12 +28,15 @@ echo -e "\n\n\033[31m   Repos:"
 echo -e "\033[0m"
 find -L . -name build.gradle -print |xargs grep -A 2 repositor
 
-echo -e "\n\n\033[31m   Prebuilts and scripts:"
+echo -e "\n\n\033[31m   Prebuilts and assets:"
 echo -e "\033[0m"
 find -L . -name \*jar | grep -v 'gradle\|build\|dexed'
-find -L . -name \*zip
-find -L . -name \*.a
-find -L . -name \*so
+find -L . -name \*zip -o -name \*.a -o -name \*so -o -name \*apk
+echo " "
+ls -R $(find -L . -name assets -o -name raw)
+
+echo -e "\n\n\033[31m   Scripts:"
+echo -e "\033[0m"
 find -L .  -maxdepth 2 -type f -executable
 
 echo -e "\n\n\033[31m   Licence:"
@@ -42,6 +47,7 @@ ls -1 LICENSE* COPYING* | xargs head -n 3
 echo -e "\n\n\033[31m   Fix:"
 echo -e "\033[0m"
 sed -i 's/-all.zip/-bin.zip/g' gradle/wrapper/gradle-wrapper.properties
-diff ~/fdroiddata/misc/gradlew gradlew
+diff ~/fdroiddata/templates/gradlew gradlew
 chmod +x gradlew
 echo " "
+
