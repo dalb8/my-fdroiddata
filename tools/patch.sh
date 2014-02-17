21ccb1f100edeec98848b0ae03f6547bff4cc8d0
David Black <david8black@gmail.com>
2014-02-17 15:06:52 +0000

    tweak fdroidserver patch

--- tools/patch.sh
+++ tools/patch.sh
@@ -1,6 +1,6 @@
-sed -i "/tarball.add(build_dir, tarname, exclude=tarexc)/d" fdroidserver/build.py
+sed -i "/tarball.add(build_dir, tarname, exclude=tarexc)/d" ~/fdroidserver/fdroidserver/build.py
 
-sed -i "s/'gradle'\]\, 'clean'/'gradle'\]\, '--help'/g" fdroidserver/build.py
-sed -i "s/'ant', 'clean'/'ant', '-version'/g" fdroidserver/build.py
+sed -i "s/'gradle'\]\, 'clean'/'gradle'\]\, '--help'/g" ~/fdroidserver/fdroidserver/build.py
+sed -i "s/'ant', 'clean'/'ant', '-version'/g" ~/fdroidserver/fdroidserver/build.py
 
-sed -i 's/"-s", dev, "install"/"install", "-r"/g' fdroidserver/install.py
+sed -i 's/"-s", dev, "install"/"install", "-r"/g' ~/fdroidserver/fdroidserver/install.py
