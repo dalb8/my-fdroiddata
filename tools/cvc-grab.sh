1ea6c1dbbc2d65e37a98f395b545187cc832e065
David Black <david8black@gmail.com>
2014-02-28 11:38:47 +0000

    update script

--- tools/cvc-grab.sh
+++ tools/cvc-grab.sh
@@ -1,12 +1,11 @@
 # Grab Current Version data from another repo
-# It only changes the CV if both repos are using the same Update Check Mode
 # Todo: check if the Repo: field is the same;  update Auto Name: field too
 
 #! /bin/bash
-for i in `grep -s -l 'Mode:Tags$' ~/shroiddata/metadata/*.txt`
+for i in `grep -s -l '#Update Check Mode:' metadata/*.txt`
 do N=`basename $i`
    CVC=$(sed -n 's/Current Version Code://p' ~/shroiddata/metadata/$N)
-   sed -i "s/Current Version Code:[.0-9]*/Current Version Code:$CVC/" `grep -s -l '#Update Check Mode:Tags' metadata/$N`
+   sed -i "s/Current Version Code:[.0-9]*/Current Version Code:$CVC/" metadata/$N`
    CV=$(sed -n 's/Current Version://p' ~/shroiddata/metadata/$N)
-   sed -i "s/Current Version:.*/Current Version:$CV/" `grep -s -l '#Update Check Mode:Tags' metadata/$N`
+   sed -i "s/Current Version:.*/Current Version:$CV/"  metadata/$N`
 done
