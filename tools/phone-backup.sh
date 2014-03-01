4bba2bd8b1c005d4730810ae9ab1abb8164578f7
David Black <david8black@gmail.com>
2014-03-01 19:57:37 +0000

    amend script

--- tools/phone-backup.sh
+++ tools/phone-backup.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
+
+# Todo: adbfs: since adb is on for adb backup, it might be convenient
+
+#adb backup -f ~/misc/p990-backup/backup-$(date +%d%m%Y).ab -noapk -all
+
 #curlftpfs -o allow_other ftp:ftp@192.168.1.103:2121/sdcard ~/phone-backup/Mémoire\ de\ stockage\ interne
-simple-mtpfs  ~/phone-backup
 #rsync -av ~/phone-backup/ ~/misc/p990-backup/Mémoire\ de\ stockage\ interne
+
+simple-mtpfs  ~/phone-backup
 rsync -a ~/phone-backup/Mémoire\ de\ stockage\ interne ~/misc/p990-backup/
 fusermount -u ~/phone-backup
-adb backup -f ~/misc/p990-backup/backup-$(date +%d%m%Y).ab -noapk -all
 
