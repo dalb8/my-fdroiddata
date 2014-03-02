4a27e98c61940fb39fca8da010c13b4b852fa2a8
David Black <david8black@gmail.com>
2014-03-02 18:17:22 +0000

    WIP DSLR

--- tools/check-fdroiddata.sh
+++ tools/check-fdroiddata.sh
@@ -1,6 +1,9 @@
 # Grab Current Version data from another repo
 # Todo: check if the Repo: field is the same;  update Auto Name: field too
 
+# Could use this to find packages that are in both repos
+#comm -12 <( find metadata/*.txt -printf '%f\n' | sort | uniq ) <( find ~/shroiddata/metadata/*.txt -printf '%f\n' | sort | uniq )
+
 #! /bin/bash
 for i in `grep -s -l '#Update Check Mode:' metadata/*.txt`
 do N=`basename $i`
