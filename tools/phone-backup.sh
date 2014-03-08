16d9c736d10bc9dd1095fb7ea9faa2a730f34aaa
David Black <david8black@gmail.com>
2014-03-08 13:52:35 +0000

    …

--- tools/phone-backup.sh
+++ tools/phone-backup.sh
@@ -7,7 +7,7 @@
 #curlftpfs -o allow_other ftp:ftp@192.168.1.103:2121/sdcard ~/phone-backup/Mémoire\ de\ stockage\ interne
 #rsync -av ~/phone-backup/ ~/misc/p990-backup/Mémoire\ de\ stockage\ interne
 
-simple-mtpfs  ~/phone-backup
+simple-mtpfs -o nonempty ~/phone-backup
 rsync -a ~/phone-backup/Mémoire\ de\ stockage\ interne ~/misc/p990-backup/
 fusermount -u ~/phone-backup
 
