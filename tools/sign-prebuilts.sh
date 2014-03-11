45f74210f95ad901c2fab99915f1b0f780d306c8
David Black <david8black@gmail.com>
2014-03-11 11:39:23 +0000

    make sign script get package name from the APK

--- tools/sign-prebuilts.sh
+++ tools/sign-prebuilts.sh
@@ -1,25 +1,22 @@
 # Copyright (c) 2014 David Black. Licensed as CCBYSA
 # See https://gitorious.org/asdgasd/my-fdroiddata
 
-# This is for signing (and installing) prebuilt apks via fdroid
-# as long as the repo has been imported via fdroid.
-# If the fdroid import fails, and the build is done from tmp/importer
-# to avoid dowloading the repo again, then set PNAME to the package
-# name manually before running this.
-# Delete all but one apks first.
+# This is for signing (and zip-aligning) prebuilt APKs via fdroid
+# (whether they're signed already or not).
+# Note the fdroid otherwise deletes APKs that don't have a
+# metadata file.
 
 #! /bin/bash
 
-#PNAME=`basename $PWD`
-#PNAME="com.example.package"
-PNAME="${PWD##*/}"
+#PNAME="${PWD##*/}"
+PNAME=$(echo "$(aapt d badging "$1" |grep -m 1 'package')" | awk -F"name='|'" '{print $2}')
+DIR=$PWD
 export DATE=$(date +%d%m%Y)
-cp $(find . -name \*.apk) ~/fdroiddata/unsigned/$PNAME\_00$DATE.apk
+cp "$1" ~/fdroiddata/unsigned/$PNAME\_00$DATE.apk
 cd ~/fdroiddata/
 touch unsigned/$PNAME\_00$DATE\_src.tar.gz
 cp -n templates/minimal.txt metadata/$PNAME.txt
 fdroid publish $PNAME
-#rm repo/$PNAME\_00$DATE\_src.tar.gz
 #adb install -r repo/$PNAME\_00$DATE.apk
-cd build/$PNAME
+cd $DIR
 
