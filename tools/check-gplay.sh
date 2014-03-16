e520885f3f1cb372ca5775dd7b2ddaaed8ccd482
David Black <david8black@gmail.com>
2014-03-16 21:59:35 +0000

    Current Version updates

--- tools/check-gplay.sh
+++ tools/check-gplay.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 fdroid checkupdates --gplay 2>&1 | tee logs/gplay-$(date +%d%m%Y).log
-sed -i '/same version/d' logs/gplay-$(date +%d%m%Y).log
+sed -i '/same version/d;/not in the Play Store/d;/Varies with device/d' logs/gplay-$(date +%d%m%Y).log
 
