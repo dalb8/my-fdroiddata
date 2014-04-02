64a9eb0f277ccc4643a245d3a551e963a681f962
David Black <david8black@gmail.com>
2014-04-02 19:38:57 +0100

    bunch of updates and CV updates

--- tools/check-fdroiddata.sh
+++ tools/check-fdroiddata.sh
@@ -12,7 +12,7 @@
 #comm -12 <( find $MYFDROIDDATA/metadata/*.txt -printf '%f\n' | sort | uniq ) <( find $FDROIDDATA/metadata/*.txt -printf '%f\n' | sort | uniq )
 
 #! /bin/bash
-for i in `grep -s -l '#Update Check Mode:' $MYFDROIDDATA/metadata/*.txt`
+for i in `grep -s -l '^#Update Check Mode:' $MYFDROIDDATA/metadata/*.txt`
 do N=`basename $i`
    CVC=$(sed -n 's/Current Version Code://p' $FDROIDDATA/metadata/$N)
    sed -i "s/Current Version Code:[.0-9]*/Current Version Code:$CVC/" $MYFDROIDDATA/metadata/$N`
