73d506f499801c1d3283d0e568ab943e73fe37a8
David Black <david8black@gmail.com>
2014-04-04 10:34:04 +0100

    script

--- tools/phone-backup.sh
+++ tools/phone-backup.sh
@@ -4,10 +4,10 @@
 
 #adb backup -f ~/misc/p990-backup/backup-$(date +%d%m%Y).ab -noapk -all
 
-#curlftpfs -o allow_other ftp:ftp@192.168.1.103:2121/sdcard ~/phone-backup/Mémoire\ de\ stockage\ interne
-#rsync -av ~/phone-backup/ ~/misc/p990-backup/Mémoire\ de\ stockage\ interne
+#curlftpfs -o allow_other ftp:ftp@192.168.1.103:2121/sdcard ~/phone-backup
+#rsync -av ~/phone-backup/ ~/misc/p990-backup
 
-simple-mtpfs -o nonempty ~/phone-backup
-rsync -a ~/phone-backup/Mémoire\ de\ stockage\ interne ~/misc/p990-backup/
+simple-mtpfs ~/phone-backup
+rsync -a ~/phone-backup ~/misc/p990-backup/
 fusermount -u ~/phone-backup
 
