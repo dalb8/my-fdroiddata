ccc51d63bea9352879480e80c5b249768c0811e1
David Black <david8black@gmail.com>
2014-06-02 12:30:34 +0100

    …

--- tools/check-repo.sh
+++ tools/check-repo.sh
@@ -9,5 +9,5 @@ for i in $(grep -s -l '^Update Check Mode:RepoMan\|^Update Check Mode:Tags' meta
 do N=$(basename --suffix=.txt $i)
    fdroid checkupdates $N 2>&1 | tee -a logs/repo-$(date +%d%m%Y).log
 done
-sed -e '/Reading/d;/up to date/d;/Finished/d' -e 's/^Processing/    Processing/' -i logs/repo-$(date +%d%m%Y).log
+sed -e '/Directory/d;/git fetch origin/d;/up to date/d;/Finished/d' -e 's/^Processing/    Processing/' -i logs/repo-$(date +%d%m%Y).log
 
