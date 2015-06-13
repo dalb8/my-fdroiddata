3d6b4a670e3816874b8423da12c5c8903cbe0663
dalb8 <david8black@gmail.com>
2015-06-13 20:02:04 +0100

    run checkupdates

--- tools/check-fdroiddata.sh
+++ tools/check-fdroiddata.sh
@@ -5,14 +5,14 @@
 # without any warranty.
 
 
-# Grab Current Version data from another repo
+# Grab Current Version data from another repo: don't forget to remove #!
 # Todo: check if the Repo: field is the same;  update Auto Name: field too
 
 # Could use this to find packages that are in both repos
 #comm -12 <( find $MYFDROIDDATA/metadata/*.txt -printf '%f\n' | sort | uniq ) <( find $FDROIDDATA/metadata/*.txt -printf '%f\n' | sort | uniq )
 
 #! /bin/bash
-for i in `grep -s -l '#Update Check Mode:' $MYFDROIDDATA/metadata/*.txt`
+for i in `grep -s -l '^#Update Check Mode:' $MYFDROIDDATA/metadata/*.txt`
 do N=`basename $i`
    CVC=$(sed -n 's/Current Version Code://p' $FDROIDDATA/metadata/$N)
    sed -i "s/Current Version Code:[.0-9]*/Current Version Code:$CVC/" $MYFDROIDDATA/metadata/$N`
