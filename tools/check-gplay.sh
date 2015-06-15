6f9a042738fe8919bbf37c268d9874a9764bed70
dalb8 <david8black@gmail.com>
2015-06-15 18:49:46 +0100

    don't check Font Installer any more

--- tools/check-gplay.sh
+++ tools/check-gplay.sh
@@ -5,6 +5,8 @@
 # without any warranty.
 
 
+# Might need to use a mid-2014 fdroidserver commit
+
 #! /bin/bash
 fdroid checkupdates --gplay 2>&1 | tee logs/gplay-$(date +%d%m%Y).log
 sed -i '/same version/d;/not in the Play Store/d;/Varies with device/d' logs/gplay-$(date +%d%m%Y).log
